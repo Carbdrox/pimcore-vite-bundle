de570dcec14c4fad22e9342f6a43133059425a84
Christian Förster <christian.foerster@ort-online.net>
2023-12-21 12:32:17 +0100

    added Pimcore version 11

---
+++ src/ViteBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace ViteBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder( 'vite_bundle' );
+
+        return $treeBuilder;
+    }
+}
